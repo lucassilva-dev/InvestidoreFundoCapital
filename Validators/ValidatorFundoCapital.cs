ae4156d9328039b2678b36b51388ea0323129298
Lucas Silva <lucassilvadeoliveira@zipmail.com.br>
2021-07-09 14:30:18 -0300

    Add files via upload

---
+++ Validators/ValidatorFundoCapital.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Uniciv.Api.Models
+{
+    public class ValidatorFundoCapital : AbstractValidator<FundoCapital>
+    {
+        public ValidatorFundoCapital()
+        {
+            RuleFor(x => x.Nome).NotNull().WithMessage("O Nome não pode ficar em branco")
+                .MinimumLength(2).WithMessage("Nome muito curto")
+                .MaximumLength(50).WithMessage("Nome muito longo");
+            RuleFor(x => x.ValorNecessario).NotNull().WithMessage("O Valor necessário é obrigatório");
+            RuleFor(x => x.ValorAtual).NotNull().WithMessage("O Valor atual é obrigatório");
+            RuleFor(x => x.ValorNecessario).GreaterThan(x => x.ValorAtual).WithMessage("O Valor Necessário não pode ser menor que o atual.");
+        }
+    }
+}
---
+++ Validators/ValidatorInvestidor.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Uniciv.Api.Models
+{
+    public class ValidatorInvestidor : AbstractValidator<Investidor>
+    {
+        public ValidatorInvestidor() 
+        {
+            RuleFor(x => x.Nome).NotNull().WithMessage("O Nome não pode ficar em branco")
+                .MinimumLength(2).WithMessage("Nome muito curto")
+                .MaximumLength(50).WithMessage("Nome muito longo");
+            RuleFor(x => x.Perfil).NotNull().WithMessage("O Perfil não pode ficar vazio")
+                .MinimumLength(5).WithMessage("Perfil muito curto")
+                .MaximumLength(100).WithMessage("Nome muito longo");
+        }
+    }
+}
