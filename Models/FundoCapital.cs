9d345f01049f91058f940c31f485f07fe9bbba07
Lucas Silva <lucassilvadeoliveira@zipmail.com.br>
2021-07-09 14:20:13 -0300

    Add files via upload

---
+++ Models/FundoCapital.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Uniciv.Api.Models
+{
+    public class FundoCapital
+    {
+        public FundoCapital()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+
+        public string Nome {get; set; }
+
+        public decimal ValorNecessario { get; set; }
+
+        public decimal ValorAtual { get; set; }
+
+        public DateTime? DataResgate {get; set; }
+    }
+}
---
+++ Models/Investidor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uniciv.Api.Models
+{
+    public class Investidor
+    {
+        public Investidor()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+        public string Nome { get; set; }
+        public string Perfil { get; set; }
+    }
+}
+
